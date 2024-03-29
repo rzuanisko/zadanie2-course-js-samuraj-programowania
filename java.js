44550d6d3d30bbf708e0f1c7f907234417173c40
rzuanisko <100361088+rzuanisko@users.noreply.github.com>
2024-03-29 19:09:38 +0100

    Initial commit

---
+++ java.js
@@ -0,0 +1,38 @@
+const sizeValue = 5;
+let size = 10;
+let grow = true;
+
+const body = document.querySelector('body');
+body.style.height = "10000px";
+
+const div = document.createElement('div');
+div.style.position = "fixed";
+div.style.top = 0;
+div.style.left = 0;
+div.style.width = "100%";
+
+body.appendChild(div);
+
+const changeHeight = function(){
+    if(grow){
+        size += sizeValue;
+        div.style.backgroundColor = "green";
+        console.log('rosnę');
+    }
+    else{
+        size -= sizeValue;
+        div.style.backgroundColor = "red";
+        console.log('maleję');
+    }
+    div.style.height = size + "px";
+
+    if(size > window.innerHeight/2 ){
+        grow = !grow;
+    }
+    else if (size == sizeValue){
+        grow = !grow;
+    }
+    
+}
+
+window.addEventListener('scroll', changeHeight);
